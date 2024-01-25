8fd9fda48bd2553b5d6ec9469e49951f454e6c26
OlehKarapysh <sebastianvell777@gmail.com>
2024-01-25 14:02:02 +0200

    Added basic routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home-page/home.component';
+import { NotFoundComponent } from './modules/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

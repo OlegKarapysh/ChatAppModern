08ec558509e319bf7f065276816bf500dd7bde09
OlehKarapysh <sebastianvell777@gmail.com>
2024-01-26 14:07:57 +0200

    Additional features

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthenticationModule } from './modules/authentication/authentication.module';
-import { HomeModule } from './modules/home/home.module';
+import { NotFoundModule } from './modules/not-found/not-found.module';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         AppRoutingModule,
-        AuthenticationModule,
-        HomeModule,
+        NotFoundModule,
     ],
     providers: [],
     bootstrap: [AppComponent],

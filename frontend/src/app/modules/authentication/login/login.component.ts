067c79ed90fc348ba33cb4038c90b55e82540551
OlehKarapysh <sebastianvell777@gmail.com>
2024-01-26 21:11:02 +0200

    Added login page

--- frontend/src/app/modules/authentication/login/login.component.ts
+++ frontend/src/app/modules/authentication/login/login.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { LoginDto } from '../../../models/login-dto';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from '../../../validations/custom-validators';
 
 @Component({
     selector: 'chat-login',
@@ -6,4 +9,44 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     styleUrl: './login.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+    public loginDto: LoginDto | undefined;
+
+    public loginForm: FormGroup = new FormGroup({});
+
+    public isPasswordHidden = true;
+
+    constructor(private readonly formBuilder: FormBuilder) {}
+
+    public ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            email: [
+                '',
+                [
+                    Validators.required,
+                    Validators.email,
+                    Validators.minLength(3),
+                    Validators.maxLength(100),
+                ],
+            ],
+            password: [
+                '',
+                [
+                    Validators.required,
+                    Validators.minLength(6),
+                    Validators.maxLength(16),
+                    CustomValidators.checkPasswordStrong(),
+                ],
+            ],
+        });
+    }
+
+    public login(): void {
+        this.loginDto = {
+            email: this.loginForm.value.email,
+            password: this.loginForm.value.password,
+        };
+
+        console.log(this.loginDto);
+    }
+}
